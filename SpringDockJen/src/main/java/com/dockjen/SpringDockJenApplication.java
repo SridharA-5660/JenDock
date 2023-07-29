2add4c5497eaf48f575cfc1b8bee99cab31ab0d8
sridharkent <sridharsri5660@gmail.com>
2023-07-29 10:13:00 +0530

    spring-docker-jenkins

---
+++ SpringDockJen/src/main/java/com/dockjen/SpringDockJenApplication.java
@@ -0,0 +1,30 @@
+package com.dockjen;
+
+import java.util.Random;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class SpringDockJenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockJenApplication.class, args);
+	}
+
+}
+
+@Controller
+class IndexController{
+	
+	static String randomWebAppID= new String(new Random().toString());
+	
+	@GetMapping("/")
+	@ResponseBody
+	public String index() {
+		return "[WEBAPPID "+ randomWebAppID + "] Hello World from Spring";
+	}
+}
---
+++ SpringDockJen/src/test/java/com/dockjen/SpringDockJenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockjen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockJenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
